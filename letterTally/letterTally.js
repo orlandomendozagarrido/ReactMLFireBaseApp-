0df32e0e2e5e1e4e951d7bc7018f5d228a08748b
yidahc <yidah.a.curiel@gmail.com>
2019-02-14 09:50:03 -0600

    i think i finished the letterTally problem

--- letterTally/letterTally.js
+++ letterTally/letterTally.js
@@ -1,8 +1,21 @@
 //Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
 
-var letterTally = function(str, obj = {}) {
-  // Your code here.
-  /* START SOLUTION */
-  /* END SOLUTION */
+var letterTally = function(str) {
+  var newObj = {};  // create placeholder for return object
+  for (i = 0; i < str.length; i++) {  // loop through string to accesss each letter
+    if (!newObj[str[i]]) {   // check if that letter is a key in your object
+      newObj[str[i]] = 1;  // create property for each new letter you see
+    } else {
+      newObj[str[i]]++
+    }  // add count to each non-new letter you see
+  }
+// return object
+  return newObj;
 };
+
+/*
+I - string
+O - object with a tally of each letter
+
+console.log(letterTally("potato"));
