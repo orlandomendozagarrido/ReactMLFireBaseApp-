f83bc1b9e988a7961a376e0ca6d3e2064edbcfe8
lenholacode <len@holacode.com>
2019-02-15 11:00:16 -0600

    swamp in pairs

---
+++ swapInPairs/swapInPairs.js
@@ -0,0 +1,10 @@
+/*Swap all elements in pairs. If the length of the list is odd, the last element stays
+
+in place. [2,7,8,3,1,4] Returns [7,2,3,8,4,1] [3,6,8,1,5] Returns [6,3,1,8,5]*/
+
+
+var swapElements = function(array,i,result) {
+  // Your code here.
+  /* START SOLUTION */
+  /* END SOLUTION */
+};
