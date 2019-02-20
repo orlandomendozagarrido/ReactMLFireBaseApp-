e3fba7aba1319a609f3798622dfe1ed2fd15530b
lenholacode <len@holacode.com>
2019-02-20 14:43:50 -0600

    stack

---
+++ stack/stack.js
@@ -0,0 +1,15 @@
+/**
+ * Write a stack using your preferred instantiation pattern.
+ * Avoid using native array methods i.e., push, pop, and length.
+ * Once you're done, implement a queue using two stacks.
+ */
+
+/**
+  * Stack Class
+  */
+
+  var Stack = function() {
+    // Your code here.
+    /* START SOLUTION */
+    /* END SOLUTION */
+};
