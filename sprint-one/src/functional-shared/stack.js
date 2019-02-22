bf4c4fb7621bed4f4a84865cbc688a73dea9d9c4
Carlosf96 <carlos.f.roque96@gmail.com>
2019-02-21 20:00:16 -0600

    added a more code to functions with IvanCarrilloSolache

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {}
+  instance.storage = {}// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  instance.c = 0;// but try not not reference your old code in writing the new style.
+ extend(instance, stackMethods)
+return instance
 };
-
-var stackMethods = {};
+var extend = function(obj, methods) {
+  for (let key in methods) {
+    obj[key] = methods[key]
+  }
+}
+var stackMethods = {
+  push: function (value) {
+    var res = instance.storage[instance.c++]=value
+    return res;
+  },
+  pop: function() {
+    if(instance.c === 0)
+    return;
+  },
+  size: function() {
+    return instance.c;
+  }
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,31 +2,28 @@ var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  someInstance.storage = {};
+  someInstance.storage = {};//a storage prop in someintance obj
   someInstance.start = 0
   someInstance.end = 0
   // Implement the methods below
 
   someInstance.enqueue = function (value) {
-    someInstance.storage[someInstance.end++] = value
+    someInstance.storage[someInstance.end] = value
+    someInstance.end++
   };
 
   someInstance.dequeue = function () {
-    var size = someInstance.start-someInstance.end
-    if(size<0)return undefined;
+    var size = someInstance.start - someInstance.end
+    if (size === 0){ return undefined;}
     //if (size <= 0) return undefined;
     var item = someInstance.storage[someInstance.start];
-      delete someInstance.storage[someInstance.start];
-    someInstance.storage[someInstance.start++]
-    if (someInstance.start === someInstance.end){
-      someInstance.start = 0;
-      someInstance.end = 0;
-    }
+    delete someInstance.storage[someInstance.start];
+    someInstance.start++
     return item;
   };
 
   someInstance.size = function () {
-    return someInstance.start-someInstance.end
+    return (someInstance.end - someInstance.start)
   };
 
   return someInstance;
