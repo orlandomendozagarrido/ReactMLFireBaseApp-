6e636efc4c9aff65675ad5ffa4fd996ba233eec1
Carlosf96 <carlos.f.roque96@gmail.com>
2019-02-22 16:43:34 -0600

    Finished implementing Stack using functional shared patter

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,27 +1,29 @@
 var Stack = function() {
-  var instance = {}
+  var instance = {};
   instance.storage = {}// Hey! Rewrite in the new style. Your code will wind up looking very similar,
   instance.c = 0;// but try not not reference your old code in writing the new style.
- extend(instance, stackMethods)
+ extend(instance, stackMethods)//extends props of our method obj to our instance
 return instance
 };
-var extend = function(obj, methods) {
-  for (let key in methods) {
+var extend = function(obj, methods) {//adds props of one obj to another obj
+  for (var key in methods) {
     obj[key] = methods[key]
   }
 }
-var stackMethods = {
-  push: function (value) {
-    var res = instance.storage[instance.c++]=value
+var stackMethods = {//methods we created that we can use on stack class
+  push: function (value) {//push function pushes the value to the stack
+    var res = this.storage[this.c++]=value
     return res;
   },
-  pop: function() {
-    if(instance.c === 0)
+  pop: function() {//pops off from stack
+    if(this.c === 0)
     return;
+    this.c--
+    var rslts = this.storage[this.c]
+    return rslts;
   },
-  size: function() {
-    return instance.c;
+  size: function() {//reports size of stack 
+    return this.c;
+    
   }
 };
-
-
