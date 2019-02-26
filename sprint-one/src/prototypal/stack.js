6d426e0657127050aea3491488191ed878534d21
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-02-25 19:57:51 -0600

    take one

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+
+  obj.storage = {};  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  obj.counter = 0;// but try not not reference your old code in writing the new style.
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function () {
 
+  },
 
+  pop: function () {
+
+  },
+
+  size: function () { // expect(stack.size()).to.equal(0);
+    return obj.counter;
+  }
+};
