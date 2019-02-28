0153c481d583817ca0f5450f73946dd6a778ddd4
lenholacode <len@holacode.com>
2019-02-27 18:00:33 -0600

    tree

---
+++ tree /tree.js
@@ -0,0 +1,30 @@
+/* Implement a tree using prototypal instantiation.
+Your tree should have methods named "addChild" and "contains".
+*/
+
+// EXAMPLE USAGE:
+// var tree = treeMaker();
+// tree.addChild(1);
+// tree.addChild(2);
+// tree.contains(2);   // yields 'true'
+
+var treeMaker = function(value) {
+  //tree code goes here!
+  /* START SOLUTION */
+  /* END SOLUTION */
+};
+
+//methods go here!
+treeMaker.methods = {};
+
+treeMaker.methods.addChild = function(/*START SOLUTION*/ value /*END SOLUTION*/) {
+  /* START SOLUTION */
+
+  /* END SOLUTION */
+};
+
+treeMaker.methods.contains = function(/*START SOLUTION*/ value /*END SOLUTION*/) {
+  /* START SOLUTION */
+
+  /* END SOLUTION */
+};
