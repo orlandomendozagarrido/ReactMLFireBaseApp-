0918604bde117d25cba7eaf3d950005215e0c39e
marcocastillo2506 <marcocastillo2506@gmail.com>
2019-03-21 07:30:38 -0600

    adding eventingLibrary

---
+++ eventingLibrary/eventingLibrary.js
@@ -0,0 +1,39 @@
+/*
+ * Make an eventing system mix-in that adds .trigger() and .on() to any input
+ * object.
+ *
+ * Example usage:
+ * var obj = mixEvents({ name: 'Alice', age: 30 });
+ * obj.on('ageChange', function(){ // On takes an event name and a callback function
+ *    console.log('Age changed');
+ * });
+ * obj.age++;
+ * obj.trigger('ageChange'); // This should call our callback! Should log 'age changed'.
+ *
+ * Caveats:
+ * - mixEvents should return the original object it was passed after extending it.
+ * - If we repeatedly call .on with the same event name, it should
+ *   continue to call the old function as well. That is to say, we can have multiple
+ *   listeners for an event.
+ * - If `obj.trigger` is called with additional arguments, pass those to the
+ *   listeners.
+ * - It is not necessary to write a way to remove listeners.
+ */
+
+var mixEvents = function(obj) {
+  // TODO: Your code here
+  /* START SOLUTION */
+  var events = {};
+
+  obj.trigger = function (event) {
+    // TODO: Your code here
+  };
+
+  // Register a callback to be fired on this event.
+  obj.on = function (event, callback) {
+    // TODO: Your code here
+  };
+
+  /* END SOLUTION */
+  return obj;
+};
